9ce2ee5b339a4e74a73d85f63f22cadc33f1fd2d
haun <ryuhaun0321@gmail.com>
2022-11-17 14:05:49 +0900

    1117 back 2

--- BACK/movies/models.py
+++ BACK/movies/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     release_date = models.DateField()
@@ -14,3 +14,11 @@ class Movie(models.Model):
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
     genres=models.ManyToManyField(Genre)
+
+# class Review(models.Model):
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add = True)
+#     updated_at = models.DateTimeField(auto_now = True)
+#     movie = models.ForeignKey(Movie, related_name = 'reviews')
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'reviews')
