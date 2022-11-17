7bcb797fa69be59686c3682a87d6aa87d997d506
haun <ryuhaun0321@gmail.com>
2022-11-17 17:42:58 +0900

    1117 back final

--- BACK/accounts/models.py
+++ BACK/accounts/models.py
@@ -4,5 +4,5 @@ from movies.models import Movie, Review
 
 # Create your models here.
 class User(AbstractUser):
-    like_movies = models.ManyToManyField(Movie, related_name = 'like_users')
-    like_reviews = models.ManyToManyField(Review, related_name = 'like_users')
+    like_movies = models.ManyToManyField(Movie, related_name = 'movie_like_users') 
+    like_reviews = models.ManyToManyField(Review, related_name = 'review_like_users')
