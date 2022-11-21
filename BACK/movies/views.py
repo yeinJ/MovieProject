88938b0eb51d1f96e52d884164db95dc283332c0
haun <ryuhaun0321@gmail.com>
2022-11-21 17:52:59 +0900

    1121 recommend 하는중

--- BACK/movies/views.py
+++ BACK/movies/views.py
@@ -8,6 +8,56 @@ from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Movie, Review
 from .serializers import MovieSerializer, ReviewSerializer
+from accounts.serializers import UserSerializer
+
+import pandas as pd
+import numpy as np
+import csv
+
+
+def find_sim_movie(idx, sorted_ind, top_n=10):
+    # sorted_ind 인자로 입력된 genre_sim_sorted_ind 객체에서 유사도 순으로 top_n 개의 index 추출
+    similar_indexes = sorted_ind[idx, :(top_n)]
+    
+    #dataframe에서 index로 사용하기 위해서 1차원 array로 변경
+    similar_indexes = similar_indexes.reshape(-1)
+
+    # index 반환
+    return similar_indexes.tolist()
+
+
+# 추천 영화 목록 조회
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def movie_recommend(request):
+    simi_path = 'C:/Users/multicampus/Desktop/FINAL_PJT/PJT_GITHUB/BACK/movies/fixtures/sim_df2.csv'
+
+    simi_df = pd.read_csv(simi_path, encoding='utf-8')
+    simi_df = simi_df.set_index('movie_id', drop=True)
+
+    simi_list = simi_df.values.tolist()
+    simi_np = np.array(simi_list)
+    simi_sorted_ind = simi_np.argsort()[:, ::-1]
+
+
+    serializer = UserSerializer(request.user)
+    print(serializer.data)
+    like_movies_idx = [movie['id'] for movie in serializer.data['like_movies']]
+    
+    recommend_movie_idx = []
+    for idx in like_movies_idx:
+        recommend_movie_idx += find_sim_movie(idx, simi_sorted_ind)
+    
+    recommend_movie_idx = [idx for idx in recommend_movie_idx if idx not in like_movies_idx]
+    recommend_movie_idx = set(recommend_movie_idx)
+    recommend_movie_idx = sorted(recommend_movie_idx)
+
+    
+    print(like_movies_idx)
+    print(recommend_movie_idx)
+    
+    return Response(True)
+
 
 # 전체 영화 목록 조회
 @api_view(['GET'])
@@ -15,12 +65,6 @@ def movie_list(request):
     movies = get_list_or_404(Movie)
     serializer = MovieSerializer(movies, many=True)
     return Response(serializer.data)
-
-
-# 추천 영화 목록 조회
-@api_view(['GET'])
-def movie_recommend(request):
-    pass
 
 
 # 단일 영화 detail 조회
