96e9da2f1192e2db2d31a1962d7ee2e6b2daf384
haun <ryuhaun0321@gmail.com>
2022-11-22 17:37:37 +0900

    1122 haun final

--- FRONT/src/components/Review/ReviewSet.vue
+++ FRONT/src/components/Review/ReviewSet.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="board-list">
-    <div class="common-buttons">
-      <ReviewForm />
-      <!-- <b-button v-bind:to="{name :'ReviewForm'}">후기 작성</b-button> -->
-    </div>
-    <table id="my-table" class="table">
+    <table id="my-table" class="table" style="width:100%">
       <thead>
-        <!-- <tr>
-        <th>번호</th>
-        <th>제목</th>
-        <th>작성자</th>
-        <th>날짜</th>
-        <th>좋아요수</th>
-      </tr>    -->
+        <th>Review</th>
+        <th><ReviewForm /></th> 
       </thead>
       <tbody>
-        <div v-for="review in perReviews" v-bind:key="review.id">
+        <tr v-for="review in perReviews" v-bind:key="review.id" style="width:100%">
           <ReviewList
-            v-bind:id="review?.id"
-            v-bind:title="review?.title"
-            v-bind:user="review?.user"
-            v-bind:created_at="review?.created_at"
-            v-bind:review_like_user_count="review?.review_like_users_count"
             v-bind:review="review"
           />
-        </div>
+        </tr>
       </tbody>
 
       <b-pagination
@@ -36,18 +22,10 @@
       ></b-pagination>
     </table>
 
-    
-
-
-
-
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
-// const API_URL = 'http://127.0.0.1:8000'
-
 import ReviewList from "@/components/Review/ReviewList.vue";
 import ReviewForm from "@/components/Review/ReviewForm.vue";
 
@@ -57,7 +35,6 @@ export default {
       currentPage: 1,
       perPage: 15, //페이지 당 보여줄 갯수
       items: this.reviews,
-      // userReviews: []
     };
   },
   components: {
@@ -77,32 +54,13 @@ export default {
       return newReviews;
     },
   },
-  created() {
-    // this.myPage()
-  },
-  methods: {
-    // myPage() {
-    //   if (this.$store.state.token) {
-    //     axios({
-    //       method: 'get',
-    //       url : `${API_URL}/mypage/`,
-    //       headers: {
-    //           Authorization: `Token ${this.$store.state.token}`
-    //       }
-    //     })
-    //       .then((res) =>{
-    //         this.userReviews = res.data.reviews
-    //         console.log(this.userReviews)
-    //       })
-    //       .catch((err) => {console.log(err)})
-    //   }
-    // }
-  },
 };
 </script>
 
 <style scoped>
 .table {
   color: white;
+  margin-top:2%;
 }
+
 </style>
